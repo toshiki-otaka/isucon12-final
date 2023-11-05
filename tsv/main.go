8d0cc168f8bf0a08d2dd6b630cf8448fdb2a699b
Masaya Hayashi <mcdonalds.only@gmail.com>
2023-11-05 17:09:09 +0900

    split tsv

--- tsv/main.go
+++ tsv/main.go
@@ -5,11 +5,23 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 )
 
 func main() {
-	f, err := os.Open("webapp/sql/5_user_presents_not_receive_data.tsv")
+	if len(os.Args) != 2 {
+		panic("specify shard count")
+	}
+	cnt, err := strconv.Atoi(os.Args[1])
 	if err != nil {
+		panic(err)
+	}
+
+	f, err := os.Open("./webapp/sql/5_user_presents_not_receive_data.tsv")
+	if err != nil {
+		fmt.Println("Run:")
+		fmt.Println("$ wget https://github.com/isucon/isucon12-final/releases/download/initial_data_20220912/initial_data.tar.gz")
+		fmt.Println("$ tar -xvf initial_data.tar.gz")
 		panic(err)
 	}
 	defer f.Close()
@@ -17,7 +29,25 @@ func main() {
 	r := csv.NewReader(f)
 	r.Comma = '\t'
 
+	writers := make([]*csv.Writer, cnt)
+	for i := 0; i < cnt; i++ {
+		f, err := os.Create("./webapp/sql/5_user_presents_not_receive_data_" + strconv.Itoa(i) + ".tsv")
+		if err != nil {
+			panic(err)
+		}
+		w := csv.NewWriter(f)
+		w.Comma = '\t'
+		defer w.Flush()
+		writers[i] = w
+	}
+
+	var line int
 	for {
+		line++
+		fmt.Println("line:", line)
+
+		first := r.InputOffset() == 0
+
 		record, err := r.Read()
 		if err == io.EOF {
 			break
@@ -26,6 +56,22 @@ func main() {
 			panic(err)
 		}
 
-		fmt.Println(record)
+		if first {
+			for _, w := range writers {
+				if err := w.Write(record); err != nil {
+					panic(err)
+				}
+			}
+			continue
+		}
+
+		userID, err := strconv.ParseInt(record[1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		i := int(userID) % cnt
+		if err := writers[i].Write(record); err != nil {
+			panic(err)
+		}
 	}
 }
